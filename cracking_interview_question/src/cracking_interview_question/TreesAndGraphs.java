6b480320ecc7e3628a604ec48b54375a9e6cfd3d
carlosl <Carlos.Lee@winmagic.com>
2016-03-07 16:27:34 -0500

    4.1

--- cracking_interview_question/src/cracking_interview_question/TreesAndGraphs.java
+++ cracking_interview_question/src/cracking_interview_question/TreesAndGraphs.java
@@ -1,6 +1,12 @@
 package cracking_interview_question;
 
 public class TreesAndGraphs {
+	
+	//4.1
+	public static boolean isBinaryTreeBalance(BST tree)
+	{
+		return false;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
