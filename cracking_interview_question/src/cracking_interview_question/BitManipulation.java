a0116602a61f1f430e1ca385e88b31ecde98e9d4
carlosl <Carlos.Lee@winmagic.com>
2016-04-06 14:19:26 -0400

    Done 5.2

--- cracking_interview_question/src/cracking_interview_question/BitManipulation.java
+++ cracking_interview_question/src/cracking_interview_question/BitManipulation.java
@@ -12,10 +12,58 @@ public class BitManipulation {
 		return result;
 	}
 	
+	//5.2
+	private static String printBinary(double num, int count)
+	{
+		String result = null;
+		double x = 0;
+		x = num * 2;
+		
+		if(count >= 32)
+		{
+			return "ERROR";
+		}
+		
+		if(x == 0)
+		{
+			return "";
+		}
+		
+		if(x > 1)
+		{
+			String s = printBinary(x-1, count+1);
+			if(s.equalsIgnoreCase("ERROR"))
+			{
+				return "ERROR";
+			}
+			result = "1" + s;
+		}
+		else
+		{
+			String s = printBinary(x, count+1);
+			if(s.equalsIgnoreCase("ERROR"))
+			{
+				return "ERROR";
+			}
+			result = "0" + s;
+		}
+
+		return result;
+	}
+	
+	public static void printBinary(double num)
+	{
+		String s = printBinary(num, 0);
+		if(s.equalsIgnoreCase("ERROR"))
+		{
+			System.out.println(s);
+		}
+		System.out.println("0." + s);
+	}
+	
 	public static void main(String[] args) 
 	{
 		System.out.println("Hello");
 	}
-	
 
 }
